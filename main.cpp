c706968e6eb204bb156d8384125fb571eedc1db4
mdziardziel <mdziardziel@gmail.com>
2019-06-02 14:56:49 +0200

    test

--- main.cpp
+++ main.cpp
@@ -119,7 +119,7 @@ bool is_my_priority_better(int sender_prev_state, int sender_timer, int sender_p
 void change_state(int new_state){
     prev_state = state;
     state = new_state;
-    // printf("%d: STAN %d -> %d, timer: %d, szatnia: %d\n", proc_id, prev_state, state, timer, room);
+    printf("%d: STAN %d -> %d, timer: %d, szatnia: %d\n", proc_id, prev_state, state, timer, room);
 }
 
 void resend_hold_messages(){
@@ -295,7 +295,7 @@ void handle_first_state(){
 
         switch(msg.type){
             case 1:
-                printf("CASE 1\n");
+                // printf("CASE 1\n");
                 if(is_my_priority_better(msg.m2, msg.m1, msg.sender)){
                     hold_messages.push(msg.sender);
                     // 
@@ -313,7 +313,7 @@ void handle_first_state(){
                 }
                 break;
             case 0:
-                            printf("CASE 0\n");
+                            // printf("CASE 0\n");
                 // printf("odbiorca: %d; nadawca: %d; typ: %d %d %d %d\n", proc_id, msg.sender, msg.type, msg.m1, msg.m2, msg.m3); 
                 received_messages++;
                 // get_zero_message[msg.sender] = 1;
